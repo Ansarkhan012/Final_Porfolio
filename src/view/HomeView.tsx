22f6e4675609557719a83dfcf09faf2705e62d96
ANSAR KHAN <ansarkhanaptech1234@gmail.com>
2026-01-18 21:57:43 +0500

    Dom-router:

--- src/view/HomeView.tsx
+++ src/view/HomeView.tsx
@@ -2,6 +2,12 @@
 
 export const HomeView= ()=> {
   return (
-    <div>HomeView</div>
+    <>
+    <section>
+      <h1>Ditching form is a mistake.
+</h1>
+<p>Great frontend development is about blending design and functionality seamlessly—where UI looks sharp, UX feels effortless, and everything just works.</p>
+    </section>
+    </>
   )
 }
